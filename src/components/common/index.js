e4d380a7fd7fd484dd54d19bb96902be6459ea91
Matej Capkovic <mcapkovic@gmail.com>
2021-04-11 13:47:44 +0200

    Add header

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as NumericSpinner } from "./NumericSpinner";
 export { default as Versus } from "./Versus";
 export { default as TeamScore } from "./TeamScore";
+export { default as Header } from "./Header";
